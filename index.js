8c83daf12415497cada5af3943e531a1e56da599
NIAKATE <mahamadouniakate@hotmail.fr>
2019-05-07 18:32:23 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost/to-do-list-app", {
+  useNewUrlParser: true
+});
+
+const Task = mongoose.model("Task", {
+  name: {
+    type: String
+  }
+});
+// **Create**
+app.post("/create", async (req, res) => {
+  try {
+    const newTask = new Task({
+      name: req.body.name
+    });
+    await newTask.save();
+    res.json({ message: "Created" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// **Read**
+app.get("/", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    console.log(tasks);
+    res.json(tasks);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// **Update**
+app.post("/update", async (req, res) => {
+  try {
+    if (req.body.id && req.body.name) {
+      const task = await Task.findOne({ _id: req.body.id });
+      // Autre manière de trouver un document à partir d'un `id` :
+      // const student = await Student.findById(req.body.id);
+      task.name = req.body.name;
+      await task.save();
+      res.json({ message: "Updated" });
+    } else {
+      res.status(400).json({ message: "Missing parameter" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// **Delete**
+app.post("/delete", async (req, res) => {
+  try {
+    if (req.body.id) {
+      const task = await Task.findOne({ _id: req.body.id });
+      // Autre manière de trouver un document à partir d'un `id` :
+      // const student = await Student.findById(req.body.id);
+      await task.remove();
+      res.json({ message: "Removed" });
+    } else {
+      res.status(400).json({ message: "Missing id" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server started");
+});
